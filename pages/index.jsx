b6866b1dcb23dce362da6f4c200f99d84215e0f5
SMUCKcyr <srmtsmigl@gmail.com>
2025-11-21 23:57:46 -0300

    Delete pages/index.jsx

--- pages/index.jsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ fontFamily: "Arial, sans-serif", padding: 40 }}>
-      <h1>SMUCK — Teste</h1>
-      <p>Se você vê isso, o build básico passou.</p>
-    </div>
-  );
-}
