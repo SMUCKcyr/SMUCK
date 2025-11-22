44c5ef2ecdc1bc26f498c9b2701789c8ac6363a8
SMUCKcyr <srmtsmigl@gmail.com>
2025-11-21 23:58:51 -0300

    Delete pages/pages/healthcheck.jsx

--- pages/pages/healthcheck.jsx
+++
@@ -1,8 +0,0 @@
-export default function Health() {
-  return (
-    <div style={{ fontFamily: "Arial, sans-serif", padding: 40 }}>
-      <h1>SMUCK — Healthcheck</h1>
-      <p>Se você vê isso, o build básico passou.</p>
-    </div>
-  );
-}
