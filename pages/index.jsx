fe6d81909a196bb04db367328a2f743a7390db7e
SMUCKcyr <srmtsmigl@gmail.com>
2025-11-30 19:51:33 -0300

    Create index.jsx

---
+++ pages/index.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ fontFamily: "Arial, sans-serif", padding: 40 }}>
+      <h1>SMUCK</h1>
+      <p>Site iniciado com sucesso ✔</p>
+      <p>Agora vamos montar tudo do zero, sem erros.</p>
+    </div>
+  );
+}
