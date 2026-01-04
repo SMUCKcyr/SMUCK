18ef5c759af21a9fb25f44a9775ad731600b8e01
SMUCKcyr <srmtsmigl@gmail.com>
2026-01-04 19:58:14 -0300

    Update whatsapp.js

--- pages/api/whatsapp.js
+++ pages/api/whatsapp.js
@@ -24,7 +24,7 @@ export default async function handler(req, res) {
 
     const payload = {
       messaging_product: "whatsapp",
-      to: "55SEUNUMEROAQUI",
+      to: "5535992362570",
       type: "text",
       text: { body: mensagem },
     };
